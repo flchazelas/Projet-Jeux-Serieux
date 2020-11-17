7233440472ee5312178c62abac4213f2abadf87e
Floran Chazelas <flo.chazelas@orange.fr>
2020-11-17 23:05:55 +0100

    Ajout Evenement

---
+++ Prototype Jeux Serieux/Assets/Scripts/Evenements/Vague.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vague : Evenement
+{
+    public int nbEnnemis;
+    public List<GameObject> listEnnemis;
+    public float interval;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        type = "Vague";
+        description = "Vague d'ennemis";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Prototype Jeux Serieux/Assets/Scripts/Tags.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tags
+{
+    public static bool CompareTags(string tag, GameObject obj)
+    {
+        string[] tabTags = obj.tag.Split(',');
+
+        foreach(string str in tabTags)
+        {
+            if(str.Equals(tag))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
