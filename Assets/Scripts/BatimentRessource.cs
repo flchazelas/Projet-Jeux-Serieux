0ab5d47068b71ceba763712a91856c9df4d439e3
Alex <alexsaffin@free.fr>
2020-11-19 16:58:57 +0100

    changement Nom

--- Assets/Scripts/BatimentRessource.cs
+++ Assets/Scripts/BatimentRessource.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GenerateResources : Batiment
+public class BatimentRessource : Batiment
 {
     public bool generateGold;
     public bool generateMeat;
